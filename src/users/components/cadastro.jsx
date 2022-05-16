f18b2323d0ccafddf85cfbbffeb8d1bcb7a853bf
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-05-16 11:45:09 -0300

    mudanças no layout de cadastro

--- src/users/components/cadastro.jsx
+++ src/users/components/cadastro.jsx
@@ -20,40 +20,38 @@ function Cadastrar() {
   }
 
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center text-white ">
-      <main>
-        <section>
-          <div className="login w-[25rem] h-[30rem] flex justify-evenly items-center flex-col  ">
-            <h2 className="text-2xl font-bold">Cadastro de usuário</h2>
-            <form className="flex flex-col space-y-3" onSubmit={enviar}>
-              <div className="box-input">
-                <label htmlFor="">nome: </label>
-                <input id="nome" type="text" placeholder="Digite seu nome" />
-              </div>
-              <div className="box-input">
-                <label htmlFor="">senha: </label>
-                <input
-                  id="senha"
-                  type="password"
-                  placeholder="Digite sua senha"
-                />
-              </div>
-              <div className="box-input">
-                <label htmlFor="">cpf: </label>
-                <input id="cpf" type="text" placeholder="Digite seu cpf " />
-              </div>
-              <button className="button" type="submit">
-                Enviar
-              </button>
-              <Link className="" to="/">
-                {' '}
-                Fazer Login{' '}
-              </Link>
-            </form>
+    <section className="flex">
+      <div className="w-[50%] ">
+        <img
+          className="h-[100vh]"
+          src="https://images.unsplash.com/photo-1652516669547-94aa2eacd6b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          alt=""
+        />
+      </div>
+      <div className=" w-[50%] h-screen flex justify-center space-y-5 items-center flex-col  ">
+        <h2 className="titulo">Cadastro de usuário</h2>
+        <form className="flex flex-col space-y-3" onSubmit={enviar}>
+          <div className="box-input">
+            <label className="subTitulo">nome: </label>
+            <input id="nome" type="text" placeholder="Digite seu nome" />
           </div>
-        </section>
-      </main>
-    </div>
+          <div className="box-input">
+            <label className="subTitulo">senha: </label>
+            <input id="senha" type="password" placeholder="Digite sua senha" />
+          </div>
+          <div className="box-input">
+            <label className="subTitulo">cpf: </label>
+            <input id="cpf" type="text" placeholder="Digite seu cpf " />
+          </div>
+          <button className="button" type="submit">
+            Enviar
+          </button>
+          <Link className="subTitulo" to="/">
+            Fazer Login
+          </Link>
+        </form>
+      </div>
+    </section>
   );
 }
 export default Cadastrar;
